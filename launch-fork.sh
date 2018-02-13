c7ec54e30407ac585dcae073a8c125fa52660d30
Joshua Henderson <joshua.henderson@microchip.com>
2018-02-13 16:37:41 -0700

    Modify launch scripts to include env

--- launch-fork.sh
+++ launch-fork.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+. /opt/ApplicationLauncher/demo.config
+
 # close stdout, stderr, stdin and double fork - it's magic
 (($@ >&- 2>&- <&- &)&)
--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+. /opt/ApplicationLauncher/demo.config
+
 eval "$@"
